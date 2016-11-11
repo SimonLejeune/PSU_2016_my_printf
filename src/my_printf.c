f0d2974e48ad46dff3559da2db1247736f2e5dca
Simon LEJEUNE <simon.lejeune@epitech.eu>
2016-11-11 18:09:37 +0100

    correction \n

--- src/my_printf.c
+++ src/my_printf.c
@@ -5,7 +5,7 @@
 ** Login   <slejeune@epitech.net>
 ** 
 ** Started on  Fri Nov 11 16:10:06 2016 Simon LEJEUNE
-** Last update Fri Nov 11 18:04:59 2016 Simon LEJEUNE
+** Last update Fri Nov 11 18:09:10 2016 Simon LEJEUNE
 */
 
 #include <stdarg.h>
@@ -22,15 +22,16 @@ int	my_printf(char *s, ...)
     {
       if (s[i] == '%')
 	{
+	  i++;
 	  if (s[i] == 'c')
 	    my_putchar(va_arg(ap, int));
 	  if (s[i] == 'S')
 	    my_putstr(va_arg(ap, char*));
 	  if (s[i] == 'd')
 	    my_put_nbr(va_arg(ap, int));
+	}
 	  if (s[i] == '\n')
 	    my_putchar('\n');
-	}
       i++;
     }
 }
